83196a5691e4b4c8fc8a9446d18c80805ff94194
vasantha44 <108451589+vasantha44@users.noreply.github.com>
2022-07-29 11:30:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Amazon from "./components/amazon";
 import Navbar from "./components/navbar";
 import Cart from "./components/cart";
